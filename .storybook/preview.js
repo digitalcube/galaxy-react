dd39580d51911145529b15390ec50f2ade2fe789
Hidetaka Okamoto <info@wp-kyoto.net>
2021-07-02 15:49:33 +0900

    chore: update storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../src/css/styles.css'
+
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
