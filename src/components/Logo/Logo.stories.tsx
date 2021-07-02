f5a902698276dacea34ad6400e2f5462cac4e548
Hidetaka Okamoto <info@wp-kyoto.net>
2021-07-02 15:50:20 +0900

    feat: Logo component

---
+++ src/components/Logo/Logo.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import { Logo, LogoProps } from './Logo'
+
+const meta: Meta = {
+  title: 'Components/Logo',
+  component: Logo,
+  parameters: {
+    controls: { expanded: true },
+  },
+};
+
+export default meta;
+
+const Template: Story<LogoProps> = args => (
+  <>
+    <Logo {...args} />
+  </>
+);
+
+// By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
+// https://storybook.js.org/docs/react/workflows/unit-testing
+export const Default = Template.bind({});
+Default.args = {
+  name: 'shifter',
+  width: 50
+}
