14ee08ad9505494e7f68860ca3929470ebabc5d1
Hidetaka Okamoto <info@wp-kyoto.net>
2021-07-06 11:24:56 +0900

    chore: remove unuse mock

--- stories/layouts/PublicPageLayout.spec.tsx
+++ stories/layouts/PublicPageLayout.spec.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { OneColumns, TwoColumns } from './PublicPageLayout.stories';
+import { OneColumns } from './PublicPageLayout.stories';
 
 describe('Nav', () => {
   it('to match snapshot [OneColumns]', () => {
     const { asFragment } = render(
       <OneColumns />
-    );
-    expect(asFragment()).toMatchSnapshot();
-  });
-  it('to match snapshot [TwoColumns]', () => {
-    const { asFragment } = render(
-      <TwoColumns {...TwoColumns.args as any}/>
     );
     expect(asFragment()).toMatchSnapshot();
   });
