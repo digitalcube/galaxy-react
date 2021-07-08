1c7e561b03ea24e20503d4dc46442d5144576253
Hidetaka Okamoto <info@wp-kyoto.net>
2021-07-08 15:25:57 +0900

    chore: prepare to publish

--- src/components/AuthForm/FeaturePromitionBlock/FeaturePromotionBlock.tsx
+++ src/components/AuthForm/FeaturePromitionBlock/FeaturePromotionBlock.tsx
@@ -1,5 +1,5 @@
 import React, { FC, ReactNode } from 'react';
-import { Link } from '../../../routings';
+import { Link } from '../../../routings/Link/Link';
 
 export type FeaturePromotionBlockProps = {
   image: ReactNode;
