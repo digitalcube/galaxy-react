bfa73a3f5263931f36efa4dd6e7c82b3c601f193
Hidetaka Okamoto <info@wp-kyoto.net>
2021-07-08 15:42:47 +0900

    some fix

--- src/components/ExternalLink/ExternalButtonLink.spec.tsx
+++ src/components/ExternalLink/ExternalButtonLink.spec.tsx
@@ -1,6 +1,5 @@
 import { render } from '@testing-library/react';
 import React from 'react';
-import { FaCircle } from 'react-icons/fa';
 import { ExternalButtonLink } from './ExternalButtonLink';
 
 describe('ExternalButtonLink', () => {
@@ -15,20 +14,6 @@ describe('ExternalButtonLink', () => {
       {
         href: 'https://example.com',
         children: 'Link',
-      },
-    ],
-    [
-      {
-        href: 'https://getshifter.io',
-        children: 'Link',
-        icon: <FaCircle className="d-block" />,
-      },
-    ],
-    [
-      {
-        href: 'https://example.com',
-        children: 'Link',
-        icon: <FaCircle className="d-block" />,
       },
     ],
   ])('given %o, should match snapshot', (args) => {
