a761a88888fef2fc7ef05ce2b0571e78b39b1874
Hidetaka Okamoto <info@wp-kyoto.net>
2021-07-16 16:43:45 +0900

    feat: nav bar fix option

--- src/routings/Nav/Navbar.tsx
+++ src/routings/Nav/Navbar.tsx
@@ -1,6 +1,18 @@
-import React, { FC, PropsWithChildren } from 'react';
+import React, { FC, PropsWithChildren, CSSProperties } from 'react';
 
-export type NavbarProps = PropsWithChildren<{}>;
-export const Navbar: FC<NavbarProps> = ({ children }) => (
-  <nav className="navbar navbar-expand-lg">{children}</nav>
-);
+export type NavbarProps = PropsWithChildren<{
+  fixed?: boolean
+}>;
+export const Navbar: FC<NavbarProps> = ({ children, fixed }) => {
+  const style: CSSProperties = fixed ? {
+    position: 'fixed',
+    zIndex: 999,
+    background: '#fff',
+    width: '100%',
+    boxShadow: '0 1px 8px rgb(69 29 57 / 10%)',
+  }: {}
+
+  return (
+    <nav className="navbar navbar-expand-lg" style={style}>{children}</nav>
+  );
+  }
