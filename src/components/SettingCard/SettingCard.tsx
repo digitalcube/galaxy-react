e8a9c84c26b1da8ad9c6f348978bf21b96c38808
Hidetaka Okamoto <info@wp-kyoto.net>
2021-07-26 18:32:55 +0900

    fix: setting card classname

--- src/components/SettingCard/SettingCard.tsx
+++ src/components/SettingCard/SettingCard.tsx
@@ -5,14 +5,16 @@ import { SettingCardTitle } from './SettingCardTitle';
 export type SettingCardProps = PropsWithChildren<{
   title?: string;
   description?: string;
+  className?: string;
 }>;
 export const SettingCard: FC<SettingCardProps> = ({
   title,
   children,
   description,
+  className,
 }) => {
   return (
-    <div className="mt-3 px-3 py-3 rounded setting-card">
+    <div className={["mt-3 px-3 py-3 rounded setting-card", className].filter(Boolean).join(' ')}>
       {title ? <SettingCardTitle>{title}</SettingCardTitle> : null}
       {description ? (
         <SettingCardDescription>{description}</SettingCardDescription>
