fecd10c89c2c879a1d8027e68dbed2179f1b7676
Hidetaka Okamoto <info@wp-kyoto.net>
2021-08-20 16:24:30 +0900

    chore: add class name on SettingCardDescription

--- src/components/SettingCard/SettingCardDescription.tsx
+++ src/components/SettingCard/SettingCardDescription.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
 
-export const SettingCardDescription: FC = ({ children }) => {
-  return <p className="mb-0 pt-2 setting-card-description">{children}</p>;
+export const SettingCardDescription: FC<{
+  className?: string;
+}> = ({ children, className }) => {
+  return <p className={`mb-0 pt-2 setting-card-description${className ? ` ${className}`: ''}`}>{children}</p>;
 };
