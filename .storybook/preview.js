7c08489ae58ccc87f34126140a113ece60f81bf0
Hidetaka Okamoto <info@wp-kyoto.net>
2021-09-09 19:48:33 +0900

    chore: set root class name seelector in Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,34 @@
 //import '../src/css/styles.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../src/scss/styles.scss'
+import {
+  addDecorator,
+  addParameters,
+} from "@storybook/react";
+import { withRootAttribute } from "storybook-addon-root-attribute";
+
+// global
+addDecorator(withRootAttribute);
+addParameters({
+  rootAttribute: {
+    root: 'html',
+    attribute: "class",
+    defaultState: {
+      name: "Shifter",
+      value: "shifter"
+    },
+    states: [
+      {
+        name: "AMIMOTO",
+        value: "amimoto"
+      },
+      {
+        name: "Default",
+        value: null
+      }
+    ]
+  }
+});
 
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
