17059a1ec694cc5c0190c487fb9db807e00bb8d5
Hidetaka Okamoto <info@wp-kyoto.net>
2021-09-13 14:18:55 +0900

    chore: href handling

--- src/components/SiteCard/SiteCardThumbnail.tsx
+++ src/components/SiteCard/SiteCardThumbnail.tsx
@@ -7,6 +7,22 @@ export type SiteCardThumbnailProps = React.DetailedHTMLProps<
   HTMLImageElement
 > & { href?: string };
 
+const SiteThumbnail: FC<SiteCardThumbnailProps> = ({ src, ...imageProps }) => {
+  if (!src) return null;
+  if (/(png|jpg|jpeg|svg)$/.test(src)) {
+    return (
+      <img
+        src={src}
+        {...imageProps}
+        width="167"
+        height="111"
+        alt="site thumbnail"
+      />
+    );
+  }
+  return <Screenshot url={src} {...imageProps} width="167" height="111" />;
+};
+
 export const SiteCardThumbnail: FC<SiteCardThumbnailProps> = ({
   src,
   href,
@@ -15,19 +31,13 @@ export const SiteCardThumbnail: FC<SiteCardThumbnailProps> = ({
   if (!src) return null;
   return (
     <div className="site-thumbnail rounded-left">
-      <Link href={`${href}`}>
-        {/(png|jpg|jpeg|svg)$/.test(src) ? (
-          <img
-            src={src}
-            {...imageProps}
-            width="167"
-            height="111"
-            alt="site thumbnail"
-          />
-        ) : (
-          <Screenshot url={src} {...imageProps} width="167" height="111" />
-        )}
-      </Link>
+      {href ? (
+        <Link href={`${href}`}>
+          <SiteThumbnail src={src} {...imageProps} />
+        </Link>
+      ) : (
+        <SiteThumbnail src={src} {...imageProps} />
+      )}
     </div>
   );
 };
