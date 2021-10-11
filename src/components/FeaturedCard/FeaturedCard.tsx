2e22c53bf0e20170fb60db4906c2ba573f6577f4
Hidetaka Okamoto <info@wp-kyoto.net>
2021-10-11 12:22:59 +0900

    chore: featuredCard comonent button link should be outlined

--- src/components/FeaturedCard/FeaturedCard.tsx
+++ src/components/FeaturedCard/FeaturedCard.tsx
@@ -33,7 +33,7 @@ export const FeaturedCard: FC<FeaturedCardProps> = ({
       )}
       {linkText && href ? (
         <div className="position-absolute guide-list-anchor">
-          <ExternalButtonLink href={href} icon={linkIcon}>
+          <ExternalButtonLink href={href} icon={linkIcon} outline>
             {linkText}
           </ExternalButtonLink>
         </div>
