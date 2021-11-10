20432b53c195d7296c8c7c3cdb50330fe0a47c85
Yoshiko Sarakai <yoshiko@digitalcube.jp>
2021-11-10 18:00:48 +0900

    fix

--- src/components/AuthForm/AuthFormHeader/AuthFormHeader.tsx
+++ src/components/AuthForm/AuthFormHeader/AuthFormHeader.tsx
@@ -6,8 +6,8 @@ export type AuthFormHeaderProps = PropsWithChildren<{
   title: string;
   link?: string;
   linkText?: string;
-	variant: 'login' | 'signup';
-	conjunction?: string;
+  variant: 'login' | 'signup';
+  conjunction?: string;
 }>;
 export const AuthFormHeader: FC<AuthFormHeaderProps> = ({
   logo,
@@ -15,8 +15,8 @@ export const AuthFormHeader: FC<AuthFormHeaderProps> = ({
   link,
   linkText,
   variant,
-	children,
-	conjunction='or',
+  children,
+  conjunction = 'or',
 }) => (
   <div className={`mb-4 pb-2 ${variant}-header`}>
     <div className={`mb-4 text-center ${variant}-icon`}>{logo}</div>
@@ -27,7 +27,7 @@ export const AuthFormHeader: FC<AuthFormHeaderProps> = ({
       <div
         className={`mt-2 font-weight-bold text-center ${variant}-switch-way`}
       >
-				{ conjunction } <Link href={link}>{linkText}</Link>
+        {conjunction} <Link href={link}>{linkText}</Link>
       </div>
     ) : null}
     {children}
