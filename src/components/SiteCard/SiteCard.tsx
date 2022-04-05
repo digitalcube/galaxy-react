136e8bb6cfa8ca966bcd01b866c88784f6f8867a
Yoshiko Sarakai <yoshiko@digitalcube.jp>
2022-04-05 15:30:45 +0900

    fix code

--- src/components/SiteCard/SiteCard.tsx
+++ src/components/SiteCard/SiteCard.tsx
@@ -3,24 +3,25 @@ import React, { FC, PropsWithChildren } from 'react';
 export type WordPressContainerStatusBadgeProps = {
   status?: string;
 };
-export const WordPressContainerStatusBadge: FC<WordPressContainerStatusBadgeProps> =
-  ({ status }) => {
-    if (!status) return null;
-    return (
-      <div className="ml-4 site-status">
-        <div className="rounded font-weight-bold c-site-status c-is-site-running">
-          WordPress Running
-        </div>
+export const WordPressContainerStatusBadge: FC<WordPressContainerStatusBadgeProps> = ({
+  status,
+}) => {
+  if (!status) return null;
+  return (
+    <div className="ml-4 site-status">
+      <div className="rounded font-weight-bold c-site-status c-is-site-running">
+        WordPress Running
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export type SiteCardProps = PropsWithChildren<{
   className?: string;
 }>;
 export const SiteCard: FC<SiteCardProps> = (props) => {
   const className = [
-    'mb-3 rounded d-md-flex align-items-start site-list-item bg-white',
+    'mb-3 rounded d-md-flex align-items-start site-list-item',
     props.className,
   ]
     .filter(Boolean)
