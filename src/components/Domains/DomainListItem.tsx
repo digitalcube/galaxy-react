9b455363dcd2c643e46d449f7baa77e38cd14eba
Yoshiko Sarakai <yoshiko@digitalcube.jp>
2022-04-05 12:34:57 +0900

    Style adjustment

--- src/components/Domains/DomainListItem.tsx
+++ src/components/Domains/DomainListItem.tsx
@@ -9,7 +9,7 @@ export const DomainListItem: FC<DomainListItemProps> = ({
   children,
 }) => {
   return (
-    <div className="domain-list-item mt-2 px-3 py-3 rounded d-flex align-items-center justify-content-between">
+    <div className="domain-list-item mt-2 px-3 py-4 rounded d-flex align-items-center justify-content-between">
       {domainName ? (
         <DomainListItemTitle>{domainName}</DomainListItemTitle>
       ) : null}
