23ca678256b7ffad5fab63c56e51e192112185c9
Yoshiko Sarakai <yoshiko@digitalcube.jp>
2022-04-07 16:29:19 +0900

    added Shifter full logo

--- src/components/Images/Logos/index.ts
+++ src/components/Images/Logos/index.ts
@@ -1,3 +1,4 @@
 export * from './Digitalcube';
 export * from './Shifter';
+export * from './Shifter-full';
 export * from './Amimoto';
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -3,8 +3,9 @@ import { useMemo } from 'react';
 import { ImageAmimotoLogo } from '..';
 import { ImageDigitalcubeLogo } from '../Images/Logos/Digitalcube';
 import { ImageShifterLogo } from '../Images/Logos/Shifter';
+import { ImageShifterLogoFull } from '../Images/Logos/Shifter-full';
 
-export type BrandName = 'shifter' | 'digitalcube' | 'amimoto';
+export type BrandName = 'shifter' | 'digitalcube' | 'amimoto' | 'shifter-full';
 export type LogoProps = {
   name: BrandName;
   width: number;
@@ -16,7 +17,9 @@ export const Logo: FC<LogoProps> = ({ name, width = 24 }) => {
       case 'digitalcube':
         return 15 / 77;
       case 'shifter':
-        return 32 / 24;
+				return 32 / 24;
+			case 'shifter-full':
+					return 99 / 32;
       case 'amimoto':
         return 32 / 32;
       default:
@@ -30,6 +33,9 @@ export const Logo: FC<LogoProps> = ({ name, width = 24 }) => {
   }
   if (name === 'amimoto') {
     return <ImageAmimotoLogo width={width} height={height} />;
+	}
+	if (name === 'shifter-full') {
+    return <ImageShifterLogoFull width={width} height={height} />;
   }
   return <ImageShifterLogo width={width} height={height} />;
 };
