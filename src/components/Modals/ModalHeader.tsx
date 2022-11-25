0bc9bf33150bc574ef3694e5dfca76d6d5f00177
Yoshiko Sarakai <yoshiko@digitalcube.jp>
2022-11-25 15:37:48 +0900

    update snapshot

--- src/components/Modals/ModalHeader.tsx
+++ src/components/Modals/ModalHeader.tsx
@@ -28,11 +28,27 @@ export const ModalHeader: FC<ModalHeaderProps> = ({
           data-dismiss="modal"
           aria-label="Close"
           onClick={() => isOpen(false)}
-				>
-					<svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<rect width="3.97315" height="18.5414" transform="matrix(0.710644 -0.703551 0.710644 0.703551 0.500183 2.79541)" fill="white"/>
-					<rect width="3.97316" height="18.5414" transform="matrix(-0.710644 -0.703551 0.710644 -0.703552 3.32355 16)" fill="white"/>
-					</svg>
+        >
+          <svg
+            width="17"
+            height="16"
+            viewBox="0 0 17 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              width="3.97315"
+              height="18.5414"
+              transform="matrix(0.710644 -0.703551 0.710644 0.703551 0.500183 2.79541)"
+              fill="white"
+            />
+            <rect
+              width="3.97316"
+              height="18.5414"
+              transform="matrix(-0.710644 -0.703551 0.710644 -0.703552 3.32355 16)"
+              fill="white"
+            />
+          </svg>
         </button>
       ) : null}
     </div>
