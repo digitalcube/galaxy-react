35ac2f1b2a08d1efb8f6d185123ee7d0d4144056
Yoshiko Sarakai <yoshiko@digitalcube.jp>
2023-01-20 11:46:36 +0900

    update settingCard

--- src/components/SettingCard/SettingCard.tsx
+++ src/components/SettingCard/SettingCard.tsx
@@ -17,7 +17,7 @@ export const SettingCard: FC<SettingCardProps> = ({
 }) => {
   return (
     <div
-      className={['mt-3 px-3 py-4 rounded setting-card bg-white', className]
+      className={['mt-4 px-3 py-4 rounded setting-card bg-white', className]
         .filter(Boolean)
         .join(' ')}
     >
